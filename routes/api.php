dbde3b147cd31abda53356da5eec3acfe8b5b9f2
Rair Francisco dos Angelos <rair.angelos@seepe.local>
2019-06-27 16:14:59 -0300

    Simplificação das rotas da API

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+/*
 $this->get('schools', 'Api\SchoolController@index');
-
 $this->post('schools', 'Api\SchoolController@store');
-
 $this->put('schools/{id}', 'Api\SchoolController@update');
-
 $this->delete('schools/{id}', 'Api\SchoolController@destroy');
+*/
+$this->apiResource('schools', 'Api\SchoolController');
